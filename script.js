6361284346eba478b8c8b0054e86791b6dc2e03f
Incerteza88 <info@incerteza.es>
2025-05-23 09:55:09 +0000

    cambios

--- script.js
+++ script.js
@@ -30,7 +30,21 @@ window.onload = () => {
   document.getElementById("dataset").textContent = JSON.stringify(marketplaceData, null, 2);
 
   // Ejercicio pendiente por resolver: resultado vacío
-  const result = [];
+const result = marketplaceData.map(seller => ({
+    ...seller,
+    orders: seller.orders.map(order =>({
+      ...order,
+      items: order.items.map(item => ({
+        ...item,
+        variant: {
+          ...item.variant,
+          //price: "$" + item.variant.price
+          price: `$${item.variant.price}`,        
+        },
+        subtotal: `$${item.variant.price * item.quantity}`,
+      }))
+    }))
+  }));
 
   // Mostrar resultado
   document.getElementById("output").textContent = JSON.stringify(result, null, 2);
